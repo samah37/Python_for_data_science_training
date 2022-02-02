496803b2837804178254686e2f9f5a8db91afaec
samah37 <hs_kansab@esi.dz>
2022-02-02 16:23:31 +0100

    Added : training method for LVQ

--- main_lvq.py
+++ main_lvq.py
@@ -3,6 +3,9 @@
 https://machinelearningmastery.com/implement-learning-vector-quantization-scratch-python/
 '''
 import numpy as np
+from math import sqrt
+from random import randrange
+from random import seed
 #Calculate distance between two vectors function
 def euclidean_distance(row1, row2):
     distance=0.0
@@ -36,7 +39,43 @@ print(bmu)
 def random_codebook(train):
     n_records = len(train)
     n_features = len(train[0])
-    codebook = [train[np.random.randrange(n_records)][i] for i in range(n_features)]
+    codebook = [train[randrange(n_records)][i] for i in range(n_features)]
     return codebook
+def train_codebooks(train, n_codebooks, lrate, epochs):
+    codebooks = [random_codebook(train) for i in range(n_codebooks)]
+    for epoch in range(epochs):
+        rate = lrate* (1.0 -(epoch/float(epochs)))
+        sum_error = 0.0
+        for row in train:
+            bmu = get_best_matching_unit(codebooks, row)
+            for i in range(len(row)-1):
+                error =  row[i] - bmu[i]
+                sum_error += error**2
+                if bmu[-1] == row[-1]:
+                    bmu[i]+= rate * error
+                else:
+                    bmu[i]-= rate * error
+        print ('>epoch=%d, lrate=%.3f, error=%.3f'% (epoch, rate, sum_error))
+    return codebooks
+# Test the training function
+
+dataset = [[2.7810836,2.550537003,0],
+	[1.465489372,2.362125076,0],
+	[3.396561688,4.400293529,0],
+	[1.38807019,1.850220317,0],
+	[3.06407232,3.005305973,0],
+	[7.627531214,2.759262235,1],
+	[5.332441248,2.088626775,1],
+	[6.922596716,1.77106367,1],
+	[8.675418651,-0.242068655,1],
+	[7.673756466,3.508563011,1]]
+learn_rate = 0.3
+n_epochs = 10
+n_codebooks = 2
+codebooks = train_codebooks(dataset, n_codebooks, learn_rate, n_epochs)
+print('Codebooks: %s' % codebooks)
+
+
+
 
 
