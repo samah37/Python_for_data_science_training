626b62a724db5c20037bd7de02a142e5655d37cc
samah37 <hs_kansab@esi.dz>
2022-02-03 10:42:20 +0100

    Finished: LVQ method

--- main_lvq.py
+++ main_lvq.py
@@ -2,79 +2,14 @@
 '''
 https://machinelearningmastery.com/implement-learning-vector-quantization-scratch-python/
 '''
-import numpy as np
-from math import sqrt
-from random import randrange
+# LVQ for the Ionosphere Dataset
 from random import seed
+from random import randrange
 from csv import reader
-#Calculate distance between two vectors function
-def euclidean_distance(row1, row2):
-    distance=0.0
-    for i in range(len(row1)-1):
-        distance+= (row1[i] - row2[i])**2
-    return np.sqrt(distance)
+from math import sqrt
 
-# Get the best BMU
-def get_best_matching_unit(codebooks, test_row):
-    distances = list()
-    for codebook in codebooks:
-        dist= euclidean_distance(codebook, test_row)
-        distances.append((codebook, dist))
-    distances.sort(key=lambda tup: tup[1])
-    return distances[0][0]
-#test
-dataset = [[2.7810836,2.550537003,0],
-	[1.465489372,2.362125076,0],
-	[3.396561688,4.400293529,0],
-	[1.38807019,1.850220317,0],
-	[3.06407232,3.005305973,0],
-	[7.627531214,2.759262235,1],
-	[5.332441248,2.088626775,1],
-	[6.922596716,1.77106367,1],
-	[8.675418651,-0.242068655,1],
-	[7.673756466,3.508563011,1]]
-test_row = dataset[0]
-bmu = get_best_matching_unit(dataset, test_row)
-print(bmu)
-#Create a random codeBook vector
-def random_codebook(train):
-    n_records = len(train)
-    n_features = len(train[0])
-    codebook = [train[randrange(n_records)][i] for i in range(n_features)]
-    return codebook
-def train_codebooks(train, n_codebooks, lrate, epochs):
-    codebooks = [random_codebook(train) for i in range(n_codebooks)]
-    for epoch in range(epochs):
-        rate = lrate* (1.0 -(epoch/float(epochs)))
-        sum_error = 0.0
-        for row in train:
-            bmu = get_best_matching_unit(codebooks, row)
-            for i in range(len(row)-1):
-                error =  row[i] - bmu[i]
-                sum_error += error**2
-                if bmu[-1] == row[-1]:
-                    bmu[i]+= rate * error
-                else:
-                    bmu[i]-= rate * error
-        print ('>epoch=%d, lrate=%.3f, error=%.3f'% (epoch, rate, sum_error))
-    return codebooks
-# Test the training function
 
-dataset = [[2.7810836,2.550537003,0],
-	[1.465489372,2.362125076,0],
-	[3.396561688,4.400293529,0],
-	[1.38807019,1.850220317,0],
-	[3.06407232,3.005305973,0],
-	[7.627531214,2.759262235,1],
-	[5.332441248,2.088626775,1],
-	[6.922596716,1.77106367,1],
-	[8.675418651,-0.242068655,1],
-	[7.673756466,3.508563011,1]]
-learn_rate = 0.3
-n_epochs = 10
-n_codebooks = 2
-codebooks = train_codebooks(dataset, n_codebooks, learn_rate, n_epochs)
-print('Codebooks: %s' % codebooks)
+# Load a CSV file
 def load_csv(filename):
     dataset = list()
     with open(filename, 'r') as file:
@@ -83,11 +18,145 @@ def load_csv(filename):
             if not row:
                 continue
             dataset.append(row)
-        return dataset
+    return dataset
+
+
+# Convert string column to float
 def str_column_to_float(dataset, column):
     for row in dataset:
-        row[column]= float(row[column].strip())
+        row[column] = float(row[column].strip())
 
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
+
+# calculate the Euclidean distance between two vectors
+def euclidean_distance(row1, row2):
+    distance = 0.0
+    for i in range(len(row1) - 1):
+        distance += (row1[i] - row2[i]) ** 2
+    return sqrt(distance)
+
+
+# Locate the best matching unit
+def get_best_matching_unit(codebooks, test_row):
+    distances = list()
+    for codebook in codebooks:
+        dist = euclidean_distance(codebook, test_row)
+        distances.append((codebook, dist))
+    distances.sort(key=lambda tup: tup[1])
+    return distances[0][0]
+
+
+# Make a prediction with codebook vectors
+def predict(codebooks, test_row):
+    bmu = get_best_matching_unit(codebooks, test_row)
+    return bmu[-1]
+
+
+# Create a random codebook vector
+def random_codebook(train):
+    n_records = len(train)
+    n_features = len(train[0])
+    codebook = [train[randrange(n_records)][i] for i in range(n_features)]
+    return codebook
+
+
+# Train a set of codebook vectors
+def train_codebooks(train, n_codebooks, lrate, epochs):
+    codebooks = [random_codebook(train) for i in range(n_codebooks)]
+    for epoch in range(epochs):
+        rate = lrate * (1.0 - (epoch / float(epochs)))
+        for row in train:
+            bmu = get_best_matching_unit(codebooks, row)
+            for i in range(len(row) - 1):
+                error = row[i] - bmu[i]
+                if bmu[-1] == row[-1]:
+                    bmu[i] += rate * error
+                else:
+                    bmu[i] -= rate * error
+    return codebooks
+
+
+# LVQ Algorithm
+def learning_vector_quantization(train, test, n_codebooks, lrate, epochs):
+    codebooks = train_codebooks(train, n_codebooks, lrate, epochs)
+    predictions = list()
+    for row in test:
+        output = predict(codebooks, row)
+        predictions.append(output)
+    return (predictions)
+
+
+# Test LVQ on Ionosphere dataset
+seed(1)
+# load and prepare data
+filename = 'ionosphere.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0]) - 1):
+    str_column_to_float(dataset, i)
+# convert class column to integers
+str_column_to_int(dataset, len(dataset[0]) - 1)
+# evaluate algorithm
+n_folds = 5
+learn_rate = 0.3
+n_epochs = 50
+n_codebooks = 20
+scores = evaluate_algorithm(dataset, learning_vector_quantization, n_folds, n_codebooks, learn_rate, n_epochs)
+print('Scores: %s' % scores)
+print('Mean Accuracy: %.3f%%' % (sum(scores) / float(len(scores))))
 
 
 
